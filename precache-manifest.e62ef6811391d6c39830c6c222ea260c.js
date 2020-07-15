fd0ebca67dba1267361a5d4f0eb73f6f5819eca6
varun-vr-13 <varuncool495@gmail.com>
2020-07-15 11:15:05 +0530

    Updates

---
+++ precache-manifest.e62ef6811391d6c39830c6c222ea260c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd79305cf0931765fd17e4cfd33ea78",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f710406eb28e39982345",
+    "url": "/my-app/static/css/main.a94df411.chunk.css"
+  },
+  {
+    "revision": "394eea69af89e145408c",
+    "url": "/my-app/static/js/2.6f24fca1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my-app/static/js/2.6f24fca1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f710406eb28e39982345",
+    "url": "/my-app/static/js/main.b86e02ae.chunk.js"
+  },
+  {
+    "revision": "833d64ac4bae055be974",
+    "url": "/my-app/static/js/runtime-main.58a6d01e.js"
+  },
+  {
+    "revision": "77bede32e2efdffb5c65caaddb64cb73",
+    "url": "/my-app/static/media/var1.77bede32.jpg"
+  },
+  {
+    "revision": "d7f8b2ed745251bb897adfa68588d26d",
+    "url": "/my-app/static/media/var2.d7f8b2ed.jpg"
+  }
+]);
